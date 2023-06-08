e0db559087f2e1ad676cbcee36be255bf912c98a
Luna Seifert <thepuddingdeity@gmail.com>
2023-06-08 13:39:22 -0500

    commit tree f939ec6d61f5c3c7a77cf8b07f8cc65e5647ef01

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name]
+      Student.where("last_name LIKE :namae OR first_name LIKE :namae", {namae: params[:name]})
+    else
+      Student.all
+    end
+      
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
